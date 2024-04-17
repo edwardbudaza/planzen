cea9704da95bdc5f41d88d89caa444c5b2a6e7a2
edwardbudaza <edward.budaza@yahoo.co.za>
2024-04-17 18:41:58 +0200

    implemented querying for favorites

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -2,13 +2,15 @@
 
 import Image from "next/image";
 import { useOrganization } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
 import { useApiMutation } from "@/hooks/use-api-mutation";
-import { toast } from "sonner";
 
 export const EmptyBoards = () => {
+  const router = useRouter();
   const { organization } = useOrganization();
   const { mutate, pending } = useApiMutation(api.board.create);
 
@@ -21,7 +23,7 @@ export const EmptyBoards = () => {
     })
       .then((id) => {
         toast.success("Board created");
-        // TODO: Redirect to board/{id}
+        router.push(`/board/${id}`)
       })
       .catch(() => toast.error("Failed to create board"));
   };
--- convex/board.ts
+++ convex/board.ts
@@ -1,8 +1,7 @@
 import {v} from "convex/values";
  
 import { mutation } from "./_generated/server";
-import { title } from "process";
-import { error } from "console";
+
 
 const images = [
     "/placeholders/1.svg","/placeholders/2.svg","/placeholders/3.svg",
@@ -54,7 +53,21 @@ export const remove = mutation({
             throw new Error("Unauthorized");
         }
 
-        // TODO: Later check to delete favorite relation as well
+        const userId = identity.subject;
+
+        const existingFavorite = await ctx.db
+            .query("userFavorites")
+            .withIndex("by_user_board", (q) =>
+                q
+                    .eq("userId", userId)
+                    .eq("boardId", args.id) 
+            )
+            .unique();
+        
+        if (existingFavorite) { 
+            await ctx.db.delete(existingFavorite._id)
+        }
+        
 
         await ctx.db.delete(args.id)
     },
@@ -112,11 +125,10 @@ export const favorite = mutation({
 
         const existingFavorite = await ctx.db
             .query("userFavorites")
-            .withIndex("by_user_board_org", (q) =>
+            .withIndex("by_user_board", (q) =>
                 q
                     .eq("userId", userId)
                     .eq("boardId", board._id)
-                    .eq("orgId", args.orgId)
             )
             .unique();
         
@@ -159,7 +171,6 @@ export const unfavorite = mutation({
                 q
                     .eq("userId", userId)
                     .eq("boardId", board._id)
-                    // TODO: check if ordId needed
             )
             .unique();
         
