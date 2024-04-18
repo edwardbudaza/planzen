8441d76465e65eb7e3567f407a7ff6f2ebebdd4d
edwardbudaza <edward.budaza@yahoo.co.za>
2024-04-18 18:42:40 +0200

    room participants functionality implementation

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = [
+  "#DC6262", 
+  "#5FA2DD", 
+  "#A865CE", 
+  "#70C18F", 
+  "#F5C364", 
+  "#DB2777"
+];
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
