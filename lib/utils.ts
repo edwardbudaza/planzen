5a6706bb32612bb6489beba090e0640baf7f49fe
edwardbudaza <edward.budaza@yahoo.co.za>
2024-04-20 03:18:41 +0200

    cursor presence implementation

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { Camera } from "@/types/canvas";
+
 const COLORS = [
   "#DC6262", 
   "#5FA2DD", 
@@ -17,3 +19,13 @@ export function cn(...inputs: ClassValue[]) {
 export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
 }
+
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera,
+){
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
